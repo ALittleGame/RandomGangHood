cda7a0f0813d96034a597782e983034ca3731b5b
chenyufei <chenyufei_fly@163.com>
2017-09-24 09:13:48 +0800

    修正部分代码，添加了一个TODO，需要初始化

--- Assets/Scripts/Interface_map.cs
+++ Assets/Scripts/Interface_map.cs
@@ -14,7 +14,7 @@ public class Interface_map : MonoBehaviour {
 		
 	}
 
-    private int[,] _MapMatrix;
+    private int[,] _MapMatrix;  // TODO: chenyufei 该对象需要初始化，否则会崩溃，可以考虑在构造函数或者Start或者某处获取二维的长度，然后new
 
     public Interface_map()
     {
@@ -24,34 +24,38 @@ public class Interface_map : MonoBehaviour {
 
 
     //接收数据-对外接口
-    public void setMap(int[,] arr, int rows, int columns)
+    public void SetMap(int[,] arr)
     {
+        int rows = arr.GetLength(0);
         for (int rowIndex = 0; rowIndex < rows; ++rowIndex)
         {
+            int columns = arr.GetLength(1);
             for (int columnIndex = 0; columnIndex < columns; ++columnIndex)
             {
                 _MapMatrix[rowIndex,columnIndex] = arr[rowIndex,columnIndex];
             }
         }
-        generateMap();
+        GenerateMap();
     }
     //清除地图-对外接口
-    public void clearMap()
+    public void ClearMap()
     {
 
     }
 
     //更新地图-对外接口
-    public void updataMap(int[,] arr, int rows, int columns)
+    public void UpdataMap(int[,] arr)
     {
+        int rows = arr.GetLength(0);
         for (int rowIndex = 0; rowIndex < rows; ++rowIndex)
         {
+            int columns = arr.GetLength(1);
             for (int columnIndex = 0; columnIndex < columns; ++columnIndex)
             {
                 _MapMatrix[rowIndex, columnIndex] = arr[rowIndex, columnIndex];
             }
         }
-        generateMap();
+        GenerateMap();
     }
     #endregion
 
@@ -59,29 +63,29 @@ public class Interface_map : MonoBehaviour {
 
 
     //生成地图
-    private void generateMap()
+    private void GenerateMap()
     {
         int rows = _MapMatrix.GetLength(0);
         int columns = _MapMatrix.GetLength(1);
         //生成地图格子
-        generateMapGrid(rows, columns);
+        GenerateMapGrid(rows, columns);
         //对每个格子加载相应资源
         for (int rowIndex = 0; rowIndex < rows; ++rowIndex)
         {
             for (int columnIndex = 0; columnIndex < columns; ++columnIndex)
             {
-                loadMatOnGrid(_MapMatrix[rowIndex,columnIndex]);
+                LoadMatOnGrid(_MapMatrix[rowIndex,columnIndex]);
             }
         }
     }
 
     //生成地图格子-私有方法
-    private void generateMapGrid(int rows, int columns)
+    private void GenerateMapGrid(int rows, int columns)
     {
 
     }
     //加载网格子上加载资源-私有方法
-    private bool loadMatOnGrid(int MatID)
+    private bool LoadMatOnGrid(int MatID)
     {
         //从库中Find资源挂在到地图格子上
         return true;
