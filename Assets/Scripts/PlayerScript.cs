e5fd2e418c4db7770f65566376f5bebee210f05c
chenyufei <chenyufei_fly@163.com>
2017-12-10 15:22:40 +0800

    角色可以移动了；下一步：界面上要生成能够看见的GameObject来表示地图点，挂上脚本，通过鼠标检测点击了哪个GameObject，拿到对应的RPointID，传进逻辑层，由逻辑层来调用Player的SetTargetPosition

--- Assets/Scripts/PlayerScript.cs
+++ Assets/Scripts/PlayerScript.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class PlayerScript : MonoBehaviour {
 
     Vector2 targetPosition = new Vector2(0,0);
+    public float speed = 0.1f;
 
     // Use this for initialization
     void Start () {
@@ -23,16 +24,33 @@ public class PlayerScript : MonoBehaviour {
         // TODO: chenyufei 以后改成不获取鼠标位置，而是获取鼠标点击的（地点）对象位置，然后把player移动过去
         if(Input.GetMouseButtonDown(0))
         {
+            bool canMove = true;
+            // TODO: 先检查界面上的目标点附近有没有“地点”，如果没有，则canMove为false
             targetPosition = mousePositionInWorld;
 
             // TODO: chenyufei 这里要通知逻辑层进行移动
         }
 
         // 移动到目标位置
-        if((Vector2)transform.position != targetPosition)
+        if(Vector2.Distance(transform.position, targetPosition) > 1)
         {
-            transform.position = targetPosition;  // FIXME: chenyufei 这里以后可以改成慢慢挪过去
+            float deltaX = targetPosition.x - transform.position.x;
+            deltaX = deltaX > 0 ? 1 : -1;
+            deltaX = transform.position.x + deltaX * speed;
+            float deltaY = targetPosition.y - transform.position.y;
+            deltaY = deltaY > 0 ? 1 : -1;
+            deltaY = transform.position.y + deltaY * speed;
+
+            Vector2 tempPos = new Vector2(deltaX, deltaY);
+            transform.position = tempPos;
         }
 
     }  // Update
+
+    public bool SetTargetPosition(float x, float y)
+    {
+        Vector2 _target = new Vector2(x, y);
+
+        return true;
+    }
 }
--- Assets/Scripts/RMap.cs
+++ Assets/Scripts/RMap.cs
@@ -3,43 +3,31 @@ using System.Collections.Generic;
 
 public class RMap
 {
-    private int[,]     path;   // 用于描述该地图地形和通路的矩阵
-    private RPoint[,]  points; // 用于记录具体每个点，与path对应
+    private int[,] path;   // 用于描述该地图地形和通路的矩阵
+    private RPoint[,] points; // 用于记录具体每个点，与path对应
 
-    
-public RMap(int xSize, int ySize)
+
+    public RMap(int xSize, int ySize)
     {
-        // TODO: path也在这里new
+        //path = new int[ySize, xSize];  // FIXME: 以后先执行这一行初始化，再调用GenPath()去随机路径
+        GenPath();  
 
-        points = new RPoint[xSize, ySize];
-        GenPath(xSize,ySize);
-        GenPoints();
+        points = new RPoint[ySize, xSize];
     }
 
     /// <summary>
     /// 生成path数组
     /// </summary>
-    private void GenPath(int xSize, int ySize)
+    private void GenPath()
     {
-        path = new int[xSize,ySize];
-        
-        //生成随机路径算法
-
-        //test
-        for(int x = 0; x < xSize; x++)
+        // TODO: 现在用固定地图，以后改成随机地图
+        path = new int[,]
         {
-            for(int y = 0; y < ySize; y++)
-            {
-                path[x, y] = 0;
-            }
-        }
-       
-        //{
-        //    { 0, 1, 0, 0, 0},
-        //    { 0, 1, 1, 1, 1},
-        //    { 1, 0, 1, 0, 0},
-        //    { 1, 1, 1, 0, 0}
-        //};
+            { 0, 1, 0, 0, 0},
+            { 0, 1, 1, 1, 1},
+            { 1, 0, 1, 0, 0},
+            { 1, 1, 1, 0, 0}
+        };
     }
 
     /// <summary>
@@ -47,31 +35,19 @@ public RMap(int xSize, int ySize)
     /// </summary>
     private void GenPoints()
     {
-        int xSize = path.GetLength(0);//行数
-        int ySize = path.GetLength(1);//列数
-        Int64 idcount = (Int64)0;
+        int ySize = path.GetLength(0);
+        int xSize = path.GetLength(1);
 
-        for(int x=0;x<xSize;x++)
+        for (int y = 0; y < ySize; y++)
         {
-            for(int y=0;y<ySize;y++)
+            for (int x = 0; x < xSize; x++)
             {
-                if(path[x,y] > 0)
+                if (path[y, x] > 0)
                 {
                     // TODO: 这里初始化的时候用一个与y,x有关的随机值传入CreatePoint，得到该点在地图上的坐标
-
-                    //test
-                    RPoint temppoint = new RPoint(idcount, x,y);
-                    points[x, y] = temppoint;//RPointFactory.Instance().CreatePoint(0, 0);
-                    idcount++;
-                    //test over
+                    points[y, x] = RPointFactory.Instance().CreatePoint(0, 0);
 
                     // TODO: 随机给这个点加上一些特性，例如门派、商店等
-                }
-                else
-                {
-                    RPoint temppoint = new RPoint(idcount, x, y);
-                    points[x, y] = temppoint;//RPointFactory.Instance().CreatePoint(0, 0);
-                    idcount++;
                 }
             }
         }
